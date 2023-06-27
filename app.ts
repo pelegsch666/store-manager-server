d5969cf48df3fe3ba96c8ef8bfafe021bb5a07cc
pelegsch666 <peleg.mazuz@gmail.com>
2023-06-27 18:39:10 +0300

    last

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ import Item from './models/Item';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import itemsRouter from './routes/items.routes';
 dotenv.config();
 
 const MONGOURL = process.env.MONGO_URL as string;
@@ -11,73 +12,11 @@ const PORT = process.env.PORT;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api/items', itemsRouter)
 
-app.post('/item', (req, res) => {
-  console.log(req.body);
-  Item.create(req.body)
-    .then(item => {
-      res.status(200).json(item);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({ message: err });
-    });
-});
+// connect to db
 
-app.delete('/items/:id', (req, res) => {
-  const itemId = req.params.id;
-  Item.deleteOne({ _id: itemId })
-    .then(result => {
-      if (result.deletedCount > 0) {
-        res.status(200).json({ message: 'Item deleted successfully' });
-      } else {
-        res.status(404).json({ message: 'Item not found' });
-      }
-    })
-    .catch(error => res.status(500).json({ error })); // Replace "YourModel" with your actual Mongoose model name and "_id" with the identifier field of your model
-});
-app.get('/items/:name', (req, res) => {
-  console.log('searching for item');
-  const itemName = req.params.name;
-  Item.findOne({ name: itemName })
-    .then(data => {
-      res.json(data);
-    })
-    .catch(error => {
-      res.status(500).json({ message: error });
-    });
-});
-
-app.get('/data', async (req, res) => {
-  Item.find()
-    .then(data => {
-      res.json(data);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({ message: err });
-    });
-});
-
-app.put('/items/:id', (req, res) => {
-  const itemId = req.params.id;
-  const updateData = req.body;
-
-  Item.findByIdAndUpdate(itemId, updateData, { new: true })
-    .then(updatedItem => {
-      if (updatedItem) {
-        res.status(200).json(updatedItem);
-      } else {
-        res.status(404).json({ message: 'Item not found' });
-      }
-    })
-    .catch(error => {
-      res
-        .status(500)
-        .json({ message: 'Error updating item', error: error.message });
-    });
-});
-
+// comment 
 const connetToDb = async () => {
   mongoose
     .connect(MONGOURL)
---
+++ routes/items.routes.ts
@@ -0,0 +1,75 @@
+import express from 'express';
+import Item from '../models/Item';
+
+const itemsRouter = express.Router();
+//get a specific item
+itemsRouter.get('/:name', (req, res) => {
+  console.log('searching for item');
+  const itemName = req.params.name;
+  Item.findOne({ name: itemName })
+    .then(data => {
+      res.json(data);
+    })
+    .catch(error => {
+      res.status(500).json({ message: error });
+    });
+});
+
+// adding item
+itemsRouter.post('/', (req, res) => {
+  console.log(req.body);
+  Item.create(req.body)
+    .then(item => {
+      res.status(200).json(item);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: err });
+    });
+});
+// delete item
+itemsRouter.delete('/:id', (req, res) => {
+  const itemId = req.params.id;
+  Item.deleteOne({ _id: itemId })
+    .then(result => {
+      if (result.deletedCount > 0) {
+        res.status(200).json({ message: 'Item deleted successfully' });
+      } else {
+        res.status(404).json({ message: 'Item not found' });
+      }
+    })
+    .catch(error => res.status(500).json({ error })); // Replace "YourModel" with your actual Mongoose model name and "_id" with the identifier field of your model
+});
+
+// get all items
+itemsRouter.get('/', async (req, res) => {
+  Item.find()
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: err });
+    });
+});
+// update item
+itemsRouter.put('/:id', async (req, res) => {
+  const itemId = req.params.id;
+  const updateData = req.body;
+
+  Item.findByIdAndUpdate(itemId, updateData, { new: true })
+    .then(updatedItem => {
+      if (updatedItem) {
+        res.status(200).json(updatedItem);
+      } else {
+        res.status(404).json({ message: 'Item not found' });
+      }
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ message: 'Error updating item', error: error.message });
+    });
+});
+
+export default itemsRouter
