3ece8adbce8592906c91cfad4399a88c6e5d4a24
pelegsch666 <peleg.mazuz@gmail.com>
2023-06-27 07:39:59 +0300

    some changes

--- app.ts
+++ app.ts
@@ -8,26 +8,19 @@ const PORT = 5150;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send([{ man: 'man' }]);
-  console.log('lets send it');
-});
-
 app.post('/item', (req, res) => {
-  try {
-    console.log(req.body);
-    const item = Item.create(req.body);
-    console.log(item);
-    console.log('here');
-    res.status(200).json(item);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: error });
-  }
+  console.log(req.body);
+  Item.create(req.body)
+    .then(item => {
+      res.status(200).json(item);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: err });
+    });
 });
 
 app.delete('/items/:id', (req, res) => {
- 
   const itemId = req.params.id;
   Item.deleteOne({ _id: itemId })
     .then(result => {
@@ -52,12 +45,14 @@ app.get('/items/:name', (req, res) => {
 });
 
 app.get('/data', async (req, res) => {
-  try {
-    const data = await Item.find(); // Replace "YourModel" with your actual Mongoose model name
-    res.json(data);
-  } catch (error) {
-    res.status(500).json({ message: error });
-  }
+  Item.find()
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: err });
+    });
 });
 
 app.put('/items/:id', (req, res) => {
@@ -80,15 +75,17 @@ app.put('/items/:id', (req, res) => {
 });
 
 const connetToDb = async () => {
-  try {
-    await mongoose.connect(
-      'mongodb+srv://drfeelgoodpeleg:pokemon@cluster0.icinodo.mongodb.net/?retryWrites=true&w=majority'
-    );
-    console.log('connected to db');
-    app.listen(PORT, () => console.log(`app running on port ${PORT}`));
-  } catch (error) {
-    console.log(error);
-  }
+  mongoose
+    .connect(
+      'mongodb+srv://drfeelgoodpeleg:pokemon@cluster0.icinodo.mongodb.net/your-database-name?retryWrites=true&w=majority'
+    )
+    .then(() => {
+      console.log('connected to db');
+      app.listen(PORT, () => console.log(`app running on port ${PORT}`));
+    })
+    .catch(error => {
+      console.log(error);
+    });
 };
 connetToDb();
 
